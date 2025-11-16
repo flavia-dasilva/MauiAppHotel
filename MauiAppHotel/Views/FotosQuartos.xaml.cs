8c78f117649ac5ebf68245da7fc90ee027f88ced
Flávia da Silva <oiflavissima@gmail.com>
2025-11-16 11:06:14 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/FotosQuartos.xaml.cs
@@ -0,0 +1,10 @@
+namespace MauiAppHotel.Views
+{
+    public partial class FotosQuartos : ContentPage
+    {
+        public FotosQuartos()
+        {
+            InitializeComponent();
+        }
+    }
+}
